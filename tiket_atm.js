e3669968cb634dcc46699f3fe92dbf785b08d894
BaiPutra <baihaqi917@gmail.com>
2022-07-20 08:01:16 +0700

    update query

--- tiket_atm.js
+++ tiket_atm.js
@@ -1,138 +1,118 @@
-const puppeteer = require('puppeteer')
-var mysql = require('mysql')
+const puppeteer = require("puppeteer");
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'scraping',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "scraping",
 });
 
 async function scrape() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-    // login session
-    await page.goto('http://portal.jke.bri.co.id/ech_admin/home');
+  // login session
+  await page.goto("http://portal.jke.bri.co.id/ech_admin/home");
 
-    await page.waitForSelector('input.form-control:nth-child(1)');
-    await page.type('input.form-control:nth-child(1)', 'admin_jkt2', { delay: 70 });
-    await page.type('input.form-control:nth-child(2)', 'password', { delay: 70 });
-    await page.click('.btn');
+  await page.waitForSelector("input.form-control:nth-child(1)");
+  await page.type("input.form-control:nth-child(1)", "admin_jkt2", {
+    delay: 70,
+  });
+  await page.type("input.form-control:nth-child(2)", "password", { delay: 70 });
+  await page.waitForTimeout(2000);
+  await page.waitForSelector(".btn");
+  await page.click(".btn");
 
-    await page.goto('http://portal.jke.bri.co.id/ech_admin/reg_tta_his', {
-        waitUntil: 'networkidle2',
+  await page.goto("http://portal.jke.bri.co.id/ech_admin/reg_tta_his", {
+    waitUntil: "networkidle2",
+  });
+
+  let data_all = [];
+  for (let i = 0; i < 70; i++) {
+    await page.waitForTimeout(3000);
+    await page.waitForSelector("#ext-gen60");
+    await page.click("#ext-gen60");
+
+    let data = await page.evaluate(() => {
+      let results = [];
+      let items = document.querySelectorAll(
+        "div.x-grid3-row > table > tbody > tr"
+      );
+      items.forEach((item) => {
+        results.push([
+          item.querySelector("td:nth-child(1)").textContent,
+          item.querySelector("td:nth-child(5)").textContent,
+          item.querySelector("td:nth-child(6)").textContent,
+          item.querySelector("td:nth-child(7)").textContent,
+          item.querySelector("td:nth-child(10)").textContent,
+          item.querySelector("td:nth-child(12)").textContent,
+          item.querySelector("td:nth-child(4)").textContent,
+          item.querySelector("td:nth-child(22)").textContent,
+          item.querySelector("td:nth-child(23)").textContent,
+          item.querySelector("td:nth-child(24)").textContent,
+        ]);
+      });
+      return results;
     });
+    data_all.push(...data);
+  }
 
-    let data_all = [];
-    for (let i = 0; i < 2; i++) {
-        await page.waitForTimeout(3000);
-        await page.waitForSelector('#ext-gen60');
-        await page.click('#ext-gen60');
-
-        let data = await page.evaluate(() => {
-            let results = [];
-            let items = document.querySelectorAll("div.x-grid3-row > table > tbody > tr");
-            items.forEach((item) => {
-                results.push([
-                    item.querySelector("td:nth-child(1)").textContent,
-                    item.querySelector("td:nth-child(5)").textContent,
-                    item.querySelector("td:nth-child(6)").textContent,
-                    item.querySelector("td:nth-child(7)").textContent,
-                    item.querySelector("td:nth-child(10)").textContent,
-                    item.querySelector("td:nth-child(12)").textContent,
-                    item.querySelector("td:nth-child(4)").textContent,
-                    item.querySelector("td:nth-child(22)").textContent,
-                    item.querySelector("td:nth-child(23)").textContent,
-                    item.querySelector("td:nth-child(24)").textContent,
-                ]);
-            });
-            return results;
-        });
-        data_all.push(...data)
-    }
-
-    await browser.close();
-    console.log('test 1');
-    return data_all;
+  await browser.close();
+  console.log("test 1");
+  return data_all;
 }
 
 con.connect(async function (err) {
-    if (err) throw err;
-    console.log('Connected');
+  if (err) throw err;
+  console.log("Connected");
 
-    var sql = 'INSERT INTO data_ATM_CRM (ticket_id, tid, bagian, rtl_problem, lokasi, kantor_cabang, input_date, last_update, rtl_eskalasi, rtl_status) VALUES ?'
-    var sql2 = `
-    DELETE FROM data_ATM_CRM WHERE id IN(
+  var sql =
+    "INSERT INTO data_ATM_CRM (ticket_id, tid, bagian, rtl_problem, lokasi, kantor_cabang, input_date, last_update, rtl_eskalasi, rtl_status) VALUES ?";
+
+  var sql2 = `
+    DELETE FROM data_ATM_CRM 
+    WHERE rtl_eskalasi = 'E-CHANNEL' OR rtl_eskalasi = NULL OR rtl_status = 'CANCEL' OR id IN(
         SELECT id FROM (SELECT id, ROW_NUMBER()
             OVER(PARTITION BY ticket_id ORDER BY ticket_id) AS row_num
         FROM data_ATM_CRM) AS temp_table WHERE row_num > 1
-    );
-    `
-    var sql3 = `
-    UPDATE data_atm_crm
-    SET kantor_cabang = CASE WHEN kantor_cabang = 'KC BOGOR PAJAJARAN' THEN '1'
-                            WHEN kantor_cabang = 'KC JKT SAHARJO' THEN '2'
-                            WHEN kantor_cabang = 'KC DEPOK' THEN '3'
-                            WHEN kantor_cabang = 'KC JKT PASAR MINGGU' THEN '4'
-                            WHEN kantor_cabang = 'KC BEKASI HARAPAN INDAH' THEN '5'
-                            WHEN kantor_cabang = 'KC PONDOK GEDE' THEN '6'
-                            WHEN kantor_cabang = 'KC BOGOR DEWI SARTIKA' THEN '7'
-                            WHEN kantor_cabang = 'KC TAMBUN' THEN '8'
-                            WHEN kantor_cabang = 'KC CIKARANG' THEN '9'
-                            WHEN kantor_cabang = 'KC CINERE' THEN '10'
-                            WHEN kantor_cabang = 'KC JKT PANGLIMA POLIM' THEN '11'
-                            WHEN kantor_cabang = 'KC CIBUBUR' THEN '12'
-                            WHEN kantor_cabang = 'KC CIBINONG' THEN '13'
-                            WHEN kantor_cabang = 'KC JKT KRAMAT JATI' THEN '14'
-                            WHEN kantor_cabang = 'KC JKT KALIBATA' THEN '15'
-                            WHEN kantor_cabang = 'KC FATMAWATI' THEN '16'
-                            WHEN kantor_cabang = 'KC PEKAYON' THEN '17'
-                            WHEN kantor_cabang = 'KC JKT GATOT SUBROTO' THEN '18'
-                            WHEN kantor_cabang = 'KC CIMANGGIS' THEN '19'
-                            WHEN kantor_cabang = 'KC BEKASI' THEN '20'
-                            WHEN kantor_cabang = 'KC JKT TB SIMATUPANG' THEN '21'
-                            WHEN kantor_cabang = 'KC CIKAMPEK' THEN '22'
-                            WHEN kantor_cabang = 'KC KARAWANG' THEN '23'
-                            WHEN kantor_cabang = 'KC PONDOK INDAH' THEN '24'
-                            WHEN kantor_cabang = 'KC JKT KEBAYORAN BARU' THEN '25'
-                            WHEN kantor_cabang = 'KC JKT RADIO DALAM' THEN '26'
-                            WHEN kantor_cabang = 'KC JKT WARUNG BUNCIT' THEN '27'
-                            WHEN kantor_cabang = 'KC PANCORAN' THEN '28'
-                        END
-    WHERE kantor_cabang IN ('KC BOGOR PAJAJARAN', 'KC JKT SAHARJO', 'KC DEPOK', 'KC JKT PASAR MINGGU', 
-        'KC BEKASI HARAPAN INDAH', 'KC PONDOK GEDE', 'KC BOGOR DEWI SARTIKA', 'KC TAMBUN', 'KC CIKARANG', 'KC CINERE', 
-        'KC JKT PANGLIMA POLIM', 'KC CIBUBUR', 'KC CIBINONG', 'KC JKT KRAMAT JATI', 'KC JKT KALIBATA', 
-        'KC FATMAWATI','KC PEKAYON', 'KC JKT GATOT SUBROTO', 'KC CIMANGGIS', 'KC BEKASI', 'KC JKT TB SIMATUPANG', 
-        'KC CIKAMPEK', 'KC KARAWANG', 'KC PONDOK INDAH', 'KC JKT KEBAYORAN BARU', 'KC JKT RADIO DALAM', 'KC JKT WARUNG BUNCIT', 
-        'KC PANCORAN');
-    `
-    var sql4 = `
+    )`;
+
+  var sql3 = `
+    UPDATE data_atm_crm t1 
+    INNER JOIN jenis_tiket t2 
+        ON t1.rtl_problem = t2.jenis_masalah
+    INNER JOIN pemasang t3
+        ON t1.kantor_cabang = t3.nama_pemasang
+    SET t1.rtl_problem = t2.id, t1.kantor_cabang = t3.id;
+    `;
+
+  var sql4 = `
     INSERT INTO tiket_atm_crm
     SELECT *
     FROM data_atm_crm
-    WHERE rtl_eskalasi = 'KANWIL BRI' OR rtl_eskalasi = 'BRISAT';
-    `
-    var data = await scrape();
-    console.log(data)
+    `;
 
-    con.query(sql, [data], function (err) {
-        if (err) throw err;
-        console.log({ count: data.length });
-    });
+  var data = await scrape();
+  console.log(data);
 
-    con.query(sql2, function (err, res) {
-        if (err) throw err;
-        console.log({ res });
-    });
+  con.query(sql, [data], function (err) {
+    if (err) throw err;
+    console.log({ count: data.length });
+  });
 
-    con.query(sql3, function (err, res) {
-        if (err) throw err;
-        console.log({ res });
-    });
+  con.query(sql2, function (err, res) {
+    if (err) throw err;
+    console.log({ res });
+  });
 
-    con.query(sql4, function (err, res) {
-        if (err) throw err;
-        console.log({ res })
-    });
-})
+  //   con.query(sql3, function (err, res) {
+  //       if (err) throw err
+  //       console.log({ res })
+  //   })
+
+  //   con.query(sql4, function (err, res) {
+  //       if (err) throw err
+  //       console.log({ res })
+  //   })
+});
--- tiket_edc.js
+++ tiket_edc.js
@@ -1,109 +1,172 @@
-const puppeteer = require('puppeteer');
-var mysql = require('mysql')
+const puppeteer = require("puppeteer");
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'scraping',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "scraping",
 });
 
-var urlBase1 = 'http://mms.bri.co.id/index.php/maintenance/cm_report_new/';
-var urlBase2 = '?id=&jenis=&user_entry=&mid=&sub_jenis=&tgl_sort=tgl_entry&tid=&kanwil=I&tgl_awal=2022-06-01&tgl_akhir=2022-06-30&status=&pemasang=&submit=Generate';
+var urlBase1 = "http://mms.bri.co.id/index.php/maintenance/cm_report_new/";
+var urlBase2 =
+  "?id=&jenis=&user_entry=&mid=&sub_jenis=&tgl_sort=tgl_entry&tid=&kanwil=I&tgl_awal=2022-07-01&tgl_akhir=2022-07-31&status=CLOSED&pemasang=&submit=Generate";
 
 async function getLinks() {
-    const links = [];
+  const links = [];
 
-    for (let i = 0; i < 2; i++) {
-        let url = urlBase1 + [i * 15] + urlBase2;
-        links.push(url);
-    }
-    // console.log(links);
-    return {
-        links,
-    };
+  for (let i = 0; i < 16; i++) {
+    let url = urlBase1 + [i * 15] + urlBase2;
+    links.push(url);
+  }
+
+  return {
+    links,
+  };
 }
 
 async function getData(url, page) {
-    // await page.setDefaultNavigationTimeout(0);
-    await page.goto(url, { waitUntil: "networkidle2" });
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-    // get the data
-    let data = await page.evaluate(() => {
-        let results = [];
-        let items = document.querySelectorAll(".tabledata > tbody > tr");
-        items.forEach((item) => {
-            results.push([
-                item.querySelector("td:nth-child(1)") && item.querySelector("td:nth-child(1)").textContent,
-                item.querySelector("td:nth-child(2)") && item.querySelector("td:nth-child(2)").textContent,
-                item.querySelector("td:nth-child(3)") && item.querySelector("td:nth-child(3)").textContent,
-                item.querySelector("td:nth-child(4)") && item.querySelector("td:nth-child(4)").textContent,
-                item.querySelector("td:nth-child(5)") && item.querySelector("td:nth-child(5)").textContent,
-                item.querySelector("td:nth-child(6)") && item.querySelector("td:nth-child(6)").textContent,
-                item.querySelector("td:nth-child(7)") && item.querySelector("td:nth-child(7)").textContent,
-                item.querySelector("td:nth-child(8)") && item.querySelector("td:nth-child(8)").textContent,
-                item.querySelector("td:nth-child(9)") && item.querySelector("td:nth-child(9)").textContent,
-                item.querySelector("td:nth-child(10)") && item.querySelector("td:nth-child(10)").textContent,
-                item.querySelector("td:nth-child(11)") && item.querySelector("td:nth-child(11)").textContent,
-                // item.querySelector("td:nth-child(12)") && item.querySelector("td:nth-child(12)").textContent,
-            ]);
-        });
-        return results;
+  // get the data
+  let data = await page.evaluate(() => {
+    let results = [];
+    let items = document.querySelectorAll(".tabledata > tbody > tr");
+    items.forEach((item) => {
+      results.push([
+        item.querySelector("td:nth-child(1)") &&
+          item.querySelector("td:nth-child(1)").textContent,
+        item.querySelector("td:nth-child(2)") &&
+          item.querySelector("td:nth-child(2)").textContent,
+        item.querySelector("td:nth-child(3)") &&
+          item.querySelector("td:nth-child(3)").textContent,
+        item.querySelector("td:nth-child(4)") &&
+          item.querySelector("td:nth-child(4)").textContent,
+        item.querySelector("td:nth-child(5)") &&
+          item.querySelector("td:nth-child(5)").textContent,
+        item.querySelector("td:nth-child(6)") &&
+          item.querySelector("td:nth-child(6)").textContent,
+        item.querySelector("td:nth-child(7)") &&
+          item.querySelector("td:nth-child(7)").textContent,
+        item.querySelector("td:nth-child(8)") &&
+          item.querySelector("td:nth-child(8)").textContent,
+        item.querySelector("td:nth-child(9)") &&
+          item.querySelector("td:nth-child(9)").textContent,
+        item.querySelector("td:nth-child(10)") &&
+          item.querySelector("td:nth-child(10)").textContent,
+        item.querySelector("td:nth-child(11)") &&
+          item.querySelector("td:nth-child(11)").textContent,
+        item.querySelector("td:nth-child(12)") &&
+          item.querySelector("td:nth-child(12)").textContent,
+      ]);
     });
+    return results;
+  });
 
-    return {
-        data,
-    }
+  return {
+    data,
+  };
 }
 
 async function scrape() {
-    var get_links = await getLinks();
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    const dataEDC = []; 
+  var get_links = await getLinks();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const dataEDC = [];
 
-    // login session
-    await page.goto('http://mms.bri.co.id/index.php/user/login');
-    await page.waitForSelector('#cboxClose');
-    await page.click('#cboxClose');
-    await page.waitForSelector('#username');
-    await page.type('#username', '00259061', { delay: 70 });
-    await page.type('#password', 'P@ssw0rdkomplek', { delay: 70 });
-    await page.click('#loginBtn');
+  // login session
+  await page.goto("http://mms.bri.co.id/index.php/user/login");
+  await page.waitForSelector("#cboxClose");
+  await page.click("#cboxClose");
+  await page.waitForSelector("#username");
+  await page.type("#username", "00259061", { delay: 70 });
+  await page.type("#password", "P@ssw0rdkomplek", { delay: 70 });
+  await page.click("#loginBtn");
 
-    for (let link of get_links.links) {
-        var data_EDC = await getData(link, page);
-        Array.prototype.push.apply(dataEDC, data_EDC.data);
-    }
+  for (let link of get_links.links) {
+    var data_EDC = await getData(link, page);
+    Array.prototype.push.apply(dataEDC, data_EDC.data);
+  }
 
-    await browser.close();
-    console.log('test 1');
-    console.log(dataEDC);
-    return dataEDC;
+  await browser.close();
+  console.log(dataEDC);
+  return dataEDC;
 }
 
 con.connect(async function (err) {
+  if (err) throw err;
+  console.log("Connected");
+
+  var sql =
+    "INSERT INTO data_edc (ticket_id, merchant, mid, tid, peruntukan, entry_ticket, update_ticket, jenis_masalah, status, kanwil, pemasang, target_hari) VALUES ?";
+  var sql2 = `
+    DELETE FROM data_edc 
+    WHERE ticket_id = 0 OR id IN(
+        SELECT id FROM (SELECT id, ROW_NUMBER()
+            OVER(PARTITION BY ticket_id ORDER BY ticket_id) AS row_num
+        FROM data_edc) AS temp_table WHERE row_num > 1
+    );
+    `;
+  var sql3 = `
+    UPDATE data_edc
+    SET pemasang = CASE WHEN pemasang = 'KC Jakarta SAHARJO' THEN 'KC JKT Saharjo'
+                        WHEN pemasang = 'KANCA DEPOK' THEN 'KC Depok'
+                        WHEN pemasang = 'KC Jakarta PASAR MINGGU' THEN 'KC JKT PASAR MINGGU'
+                        WHEN pemasang = 'KC Jakarta PANGLIMA POLIM' THEN 'KC JKT Panglima Polim'
+                        WHEN pemasang = 'KC Jakarta KRAMAT JATI' THEN 'KC JKT Kramat Jati'
+                        WHEN pemasang = 'KC Jakarta KALIBATA' THEN 'KC JKT Kalibata'
+                        WHEN pemasang = 'KC Jakarta GATOT SUBROTO' THEN 'KC JKT Gatot Subroto'
+                        WHEN pemasang = 'KC Jakarta TB SIMATUPANG' THEN 'KC JKT TB Simatupang'
+                        WHEN pemasang = 'KC Jakarta KEBAYORAN BARU' THEN 'KC JKT Kebayoran Baru'
+                        WHEN pemasang = 'KC Jakarta RADIO DALAM' THEN 'KC JKT Radio Dalam'
+                        WHEN pemasang = 'KC Jakarta WARUNG BUNCIT' THEN 'KC JKT Warung Buncit'
+                    END
+    WHERE pemasang IN ('KC Jakarta SAHARJO', 'KC DEPOK', 'KC Jakarta PASAR MINGGU',
+        'KC Jakarta PANGLIMA POLIM', 'KC Jakarta KRAMAT JATI', 'KC Jakarta KALIBATA', 
+        'KC Jakarta GATOT SUBROTO', 'KC Jakarta TB SIMATUPANG', 'KC Jakarta KEBAYORAN BARU', 
+        'KC Jakarta RADIO DALAM', 'KC Jakarta WARUNG BUNCIT', 'KANCA DEPOK');
+    `;
+
+  var sql4 = `
+    UPDATE data_edc t1 
+	INNER JOIN jenis_tiket t2 
+		ON t1.jenis_masalah = t2.jenis_masalah
+    INNER JOIN pemasang t3
+    	ON t1.pemasang = t3.nama_pemasang
+    SET t1.jenis_masalah = t2.id, t1.pemasang = t3.id;
+    `;
+
+  var sql5 = `
+    INSERT INTO tiket_edc
+    SELECT * FROM data_edc
+    `;
+
+  var data = await scrape();
+  // console.log(data);
+
+  con.query(sql, [data], function (err) {
     if (err) throw err;
-    console.log('Connected');
+    console.log({ count: data.length });
+  });
 
-    var sql = 'INSERT INTO data_edc (ticket_id, merchant, mid, tid, peruntukan, entry_ticket, update_ticket, jenis_masalah, status, kanwil, pemasang) VALUES ?';
-    // var sql2 = `
-    // DELETE FROM ticket_ATM_CRM WHERE id IN(
-    //     SELECT id FROM (SELECT id, ROW_NUMBER()
-    //         OVER(PARTITION BY ticket_id ORDER BY ticket_id) AS row_num
-    //     FROM ticket_ATM_CRM) AS temp_table WHERE row_num > 1
-    // );
-    // `
-    var data = await scrape();
-    // console.log(data);
+  con.query(sql2, function (err, res) {
+    if (err) throw err;
+    console.log({ res });
+  });
 
-    con.query(sql, [data], function (err) {
-        if (err) throw err;
-        console.log({ count: data.length });
-    });
+  con.query(sql3, function (err, res) {
+    if (err) throw err;
+    console.log({ res });
+  });
 
-    // con.query(sql2, function (err, res) {
-    //     if (err) throw err;
-    //     console.log({ res });
-    // });
-})
+  con.query(sql4, function (err, res) {
+    if (err) throw err;
+    console.log({ res });
+  });
+
+  con.query(sql5, function (err, res) {
+    if (err) throw err;
+    console.log({ res });
+  });
+});
